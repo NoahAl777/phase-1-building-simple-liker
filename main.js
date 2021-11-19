e886c890a638649a9f4bdeb28937bbc716acf479
Noah <crashlive112@gmail.com>
2021-11-19 12:40:44 -0500

    commit tree 19cd8868a42148e494e40ecbb05bc9fed0259dce

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const allHearts = document.querySelectorAll('.like-glyph');
 
+function likeCallback(e) {
+  const heart = e.target
+  mimicServerCall('exampleUrl')
+    .then(() => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = 'activated-heart'
+      }
+      else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ''
+      }
+    })
+    .catch((error) => {
+      const modal = document.querySelector('#modal');
+      modal.className = ''
+      modal.innerText = error
+      setTimeout(() => modal.className = 'hidden', 3000)
+    })
+}
+
+for (const heart of allHearts) {
+  heart.addEventListener('click', likeCallback)
+}
 
 
 
@@ -11,9 +36,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
